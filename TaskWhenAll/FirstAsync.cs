5f2bff4ac12b20b7904d8ad0cfce381ec35ee0c2
Volkan YENİBAĞCI <vyenibagci@innova.com.tr>
2024-05-16 16:02:30 +0300

    Add project files.

---
+++ TaskWhenAll/FirstAsync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskWhenAll
+{
+    public class FirstAsync
+    {
+        public async Task FirstMethodAsync()
+        {
+            for (int i = 0; i < 10; i++) { 
+            await Task.Delay(1000);
+            Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ TaskWhenAll/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskWhenAll;
+
+Console.WriteLine("TASKWHENALL!");
+var sw = new System.Diagnostics.Stopwatch();
+sw.Start();
+await new FirstAsync().FirstMethodAsync();
+await new SecondAsync().SecondMethodAsync();
+sw.Stop();
+Console.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds}ms");
+
+
+
+
+//var sw = new System.Diagnostics.Stopwatch();
+//sw.Start();
+//await Task.WhenAll(new FirstAsync().FirstMethodAsync(), new SecondAsync().SecondMethodAsync());
+//sw.Stop();
+//Console.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds}ms");
+
